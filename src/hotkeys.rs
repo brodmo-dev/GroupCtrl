499ae30da568ebbc8498d55610dbabf63509b5da
Moritz Brödel <moritz.broedel@gmail.com>
2025-12-10 14:57:21 +0200

    Add HotkeyListener to fix polling issue

--- src/hotkeys.rs
+++ src/hotkeys.rs
@@ -6,42 +6,60 @@ use std::collections::HashMap;
 use std::sync::mpsc;
 use std::thread;
 
-type HotkeyAction = Box<dyn Action + 'static>;
+pub type HotkeyAction = Box<dyn Action + 'static>;
 
-pub struct HotkeyManager {
-    _global_manager: GlobalHotKeyManager,
-    action_sender: mpsc::Sender<(u32, HotkeyAction)>,
+pub struct HotkeyListener {
+    hotkey_actions: HashMap<u32, HotkeyAction>,
+    action_receiver: mpsc::Receiver<(u32, HotkeyAction)>,
 }
 
-impl HotkeyManager {
-    fn listen_for_hotkeys(callback_receiver: mpsc::Receiver<(u32, HotkeyAction)>) {
-        let mut hotkey_actions = HashMap::new();
+impl HotkeyListener {
+    pub fn new(action_receiver: mpsc::Receiver<(u32, HotkeyAction)>) -> Self {
+        HotkeyListener {
+            hotkey_actions: HashMap::new(),
+            action_receiver,
+        }
+    }
 
+    pub fn listen_for_hotkeys(&mut self) {
+        self.update_hotkey_actions();
         loop {
-            while let Ok((id, callback)) = callback_receiver.try_recv() {
-                hotkey_actions.insert(id, callback);
-            }
             if let Ok(event) = GlobalHotKeyEvent::receiver().recv()
                 && event.state == global_hotkey::HotKeyState::Pressed
-                && let Some(action) = hotkey_actions.get(&event.id)
             {
-                action.execute()
+                self.update_hotkey_actions();
+                if let Some(action) = self.hotkey_actions.get(&event.id) {
+                    action.execute()
+                }
             }
         }
     }
 
+    fn update_hotkey_actions(&mut self) {
+        while let Ok((id, callback)) = self.action_receiver.try_recv() {
+            self.hotkey_actions.insert(id, callback);
+        }
+    }
+}
+
+pub struct HotkeyManager {
+    global_manager: GlobalHotKeyManager,
+    action_sender: mpsc::Sender<(u32, HotkeyAction)>,
+}
+
+impl HotkeyManager {
     pub fn new() -> Self {
         let manager = GlobalHotKeyManager::new().unwrap();
         let (tx, rx) = mpsc::channel();
-        thread::spawn(move || Self::listen_for_hotkeys(rx));
+        thread::spawn(move || HotkeyListener::new(rx).listen_for_hotkeys());
         Self {
-            _global_manager: manager,
+            global_manager: manager,
             action_sender: tx,
         }
     }
 
     pub fn register_hotkey<T: Action + 'static>(&self, hotkey: HotKey, action: T) -> Result<()> {
-        self._global_manager.register(hotkey)?;
+        self.global_manager.register(hotkey)?;
         self.action_sender.send((hotkey.id(), Box::new(action)))?;
         Ok(())
     }
