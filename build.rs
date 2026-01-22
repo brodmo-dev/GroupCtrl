a6ce335792fffd7d9cc533bcac3733b97a5d083f
Moritz Brödel <moritz.broedel@gmail.com>
2026-01-22 17:23:08 +0100

    Add missing npm message ref G-19

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 use std::process::Command;
 
 fn main() {
-    for path in ["package.json", "input.css", "src"] {
+    for path in ["package.json", "node_modules", "assets", "input.css", "src"] {
         println!("cargo:rerun-if-changed={}", path);
     }
-    let status = Command::new("npm").args(["run", "build"]).status();
-    if let Ok(s) = status
-        && !s.success()
-    {
+
+    let status = Command::new("npm")
+        .args(["run", "build"])
+        .status()
+        .expect("Please install npm https://nodejs.org/en/download");
+
+    if !status.success() {
         panic!("Tailwind build failed. Ensure you've run 'npm install'.");
     }
 }
