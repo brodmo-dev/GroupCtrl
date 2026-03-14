52781ce64fad4943b609c2ea7b28210c8cc0129b
Moritz Brödel <moritz.broedel@gmail.com>
2026-03-14 15:19:26 +0100

    Fix target dropdown on older macOS versions close GC-88

--- src/components/select/component.rs
+++ src/components/select/component.rs
@@ -6,7 +6,6 @@ use dioxus_primitives::select::{
 
 #[component]
 pub fn Select<T: Clone + PartialEq + 'static>(props: SelectProps<T>) -> Element {
-    use_context_provider(|| Signal::new(format!("--select-{}", uuid::Uuid::new_v4().simple())));
     rsx! {
         document::Link { rel: "stylesheet", href: asset!("./style.css") }
         select::Select {
@@ -27,11 +26,9 @@ pub fn Select<T: Clone + PartialEq + 'static>(props: SelectProps<T>) -> Element 
 
 #[component]
 pub fn SelectTrigger(props: SelectTriggerProps) -> Element {
-    let anchor = use_context::<Signal<String>>();
     rsx! {
         select::SelectTrigger {
             class: "select-trigger",
-            style: format!("anchor-name: {}", anchor()),
             attributes: props.attributes,
             {props.children}
             svg {
@@ -53,11 +50,9 @@ pub fn SelectValue(props: SelectValueProps) -> Element {
 
 #[component]
 pub fn SelectList(props: SelectListProps) -> Element {
-    let anchor = use_context::<Signal<String>>();
     rsx! {
         select::SelectList {
             class: "select-list",
-            style: format!("position-anchor: {}", anchor()),
             id: props.id,
             attributes: props.attributes,
             {props.children}
